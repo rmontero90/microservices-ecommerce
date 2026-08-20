47c9fbd918ac017525c408268af1224644413411
Rafael Montero <developer@rmontero.me>
2026-08-20 11:53:29 -0600

    Add Inventory Controller

---
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/controller/InventoryController.java
@@ -0,0 +1,50 @@
+package com.ecommerce.inventory_service.controller;
+
+import com.ecommerce.inventory_service.dto.InventoryRequestDTO;
+import com.ecommerce.inventory_service.dto.InventoryResponseDTO;
+import com.ecommerce.inventory_service.repository.InventoryRepository;
+import com.ecommerce.inventory_service.service.InventoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryRepository inventoryRepository;
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku}/{quantity}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku") String sku, @PathVariable("quantity") Integer quantity) {
+        return inventoryService.isStock(sku, quantity);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public InventoryResponseDTO createInventory(@RequestBody @Valid InventoryRequestDTO inventoryRequestDTO) {
+        return inventoryService.createInventory(inventoryRequestDTO);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponseDTO> getInventory() {
+        return inventoryService.getAllInventory();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public InventoryResponseDTO updateInventory(@PathVariable Long id, @Valid @RequestBody InventoryRequestDTO inventoryRequestDTO) {
+        return inventoryService.updateInventory(id, inventoryRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteInventory(@PathVariable Long id) {
+        inventoryService.deleteInventory(id);
+    }
+}
