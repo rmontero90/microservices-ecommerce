0269d51bbab09270fe390d5fca9d762634a959e7
Rafael Montero <developer@rmontero.me>
2026-08-20 22:16:22 -0600

    Fixes in the pom.xml and added annotations in ResponseDTO

--- inventory-service/src/main/java/com/ecommerce/inventory_service/dto/InventoryResponseDTO.java
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/dto/InventoryResponseDTO.java
@@ -1,5 +1,14 @@
 package com.ecommerce.inventory_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class InventoryResponseDTO {
     private Long id;
     private String sku;
