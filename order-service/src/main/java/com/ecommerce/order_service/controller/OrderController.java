88e65f8ce9ba4516dc047f83739899b9270d93e8
Rafael Montero <developer@rmontero.me>
2026-08-21 19:10:23 -0600

    Add Order Controller

---
+++ order-service/src/main/java/com/ecommerce/order_service/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.ecommerce.order_service.controller;
+
+import com.ecommerce.order_service.dto.OrderRequest;
+import com.ecommerce.order_service.dto.OrderResponse;
+import com.ecommerce.order_service.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponse placeOrder(@Valid @RequestBody OrderRequest orderRequest) {
+        return orderService.placeOrder(orderRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderResponse> getOrders() {
+        return  orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public OrderResponse getOrdersById(@PathVariable Long id) {
+        return  orderService.getOrderById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrderById(@PathVariable Long id) {
+        orderService.deleteOrderById(id);
+    }
+
+}
