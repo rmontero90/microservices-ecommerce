1eed06bb0cf59a451f3967405599e8f46a772917
Rafael Montero <developer@rmontero.me>
2026-08-24 17:07:01 -0600

    Add refreshScope with Actuator to products microservice

--- product-service/src/main/java/com/ecommerce/products_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/products_service/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.ecommerce.products_service.controller;
 import com.ecommerce.products_service.dto.ProductRequestDTO;
 import com.ecommerce.products_service.dto.ProductResponseDTO;
 import com.ecommerce.products_service.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +16,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/product")
 @RequiredArgsConstructor
+@RefreshScope
 public class ProductController {
 
     private final ProductService productService;
+
+    @Value("${app.maintenance.message: Sistema Operativo}")
+    private String maintenanceMessage;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -25,7 +32,8 @@ public class ProductController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponseDTO> getAllProducts() {
+    public List<ProductResponseDTO> getAllProducts(HttpServletResponse response) {
+        response.addHeader("X-Maintenance-Message", maintenanceMessage);
         return productService.getAllProducts();
     }
 
