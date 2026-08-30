132bbbdc266c01089124349643f842ad1661cc7a
Rafael Montero <developer@rmontero.me>
2026-08-30 00:24:12 -0600

    Add dynamic WebClientConfig Builder and LoadBalancer for order-service

--- order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
+++ order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.order_service.config;
 
 import com.ecommerce.order_service.service.client.InventoryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,14 +12,16 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClientBuilder() {
-        return WebClient.builder()
-                .baseUrl("http://localhost:8080")
-                .build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
     @Bean
-    public InventoryClient inventoryClient(WebClient webClient) {
+    public InventoryClient inventoryClient(WebClient.Builder builder) {
+
+        WebClient webClient = builder
+                .baseUrl("http://INVENTORY-SERVICE").build();
 
         HttpServiceProxyFactory factory =
                 HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient)).build();
