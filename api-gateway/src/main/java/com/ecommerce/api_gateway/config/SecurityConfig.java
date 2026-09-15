4786939f1c5a900eb07cd100f7f840af4346dd6a
Rafael Montero <developer@rmontero.me>
2026-09-14 23:36:48 -0600

    Add SecurityConfig

--- api-gateway/src/main/java/com/ecommerce/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ecommerce/api_gateway/config/SecurityConfig.java
@@ -4,7 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -20,5 +28,26 @@ public class SecurityConfig {
 
                 }));
         return http.build();
+    }
+
+
+    private ReactiveJwtAuthenticationConverterAdapter reactiveJwtAuthenticationConverterAdapter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Map<String, Object> realmAccess =  (Map) jwt.getClaims().get("realm_access");
+
+            if (realmAccess == null || realmAccess.isEmpty()) {
+                return  Collections.emptyList();
+            }
+
+            Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+        });
+
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
     }
 }
