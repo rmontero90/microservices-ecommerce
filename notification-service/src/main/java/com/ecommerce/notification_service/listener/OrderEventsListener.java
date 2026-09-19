0112b3f8f04221baf54380ef89860f36225ac3fb
Rafael Montero <developer@rmontero.me>
2026-09-18 18:59:15 -0600

    Add JavaMailSender with MailTrap in notificacion-service

--- notification-service/src/main/java/com/ecommerce/notification_service/listener/OrderEventsListener.java
+++ notification-service/src/main/java/com/ecommerce/notification_service/listener/OrderEventsListener.java
@@ -1,13 +1,19 @@
 package com.ecommerce.notification_service.listener;
 
 import com.ecommerce.notification_service.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class OrderEventsListener {
+
+    private final JavaMailSender mailSender;
 
     @RabbitListener(queues = "notification-queue")
     public void handleOrderPlacedEvent(OrderPlacedEvent event) {
@@ -16,6 +22,15 @@ public class OrderEventsListener {
 
         event.items().forEach(item -> {
            try {
+
+               SimpleMailMessage message = new SimpleMailMessage();
+               message.setFrom("pedidos@ecommerce.com");
+               message.setTo(event.email());
+               message.setSubject("Order Placed - "+event.orderNumber());
+               message.setText("Hello!\n\n" +
+                       "Thank for you purchase from us");
+               mailSender.send(message);
+
                log.info("Sending confirmation email to: {}", event.email());
                log.info("Email succesfully for order: {}", event.orderNumber());
            } catch (Exception e) {
